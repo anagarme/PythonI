dee39e1bf92cccda43ab819361fe8e2fb80d54e9
Ana Marisol García Mejía <124300524+anagarme@users.noreply.github.com>
2023-03-23 12:54:28 -0600

    Se añadió leyendos_archivos.py

---
+++ lessons/src/leyendos_archivos.py
@@ -0,0 +1,6 @@
+my_file_name='C:/Users/LCG/Documents/PythonI/lessons/data/dna.txt'
+my_file = open(my_file_name)
+my_dna = my_file.read()
+my_dna=my_dna.rstrip("\n")
+dna_length=len(my_dna)
+print("La secuencia es" + my_dna + "y su longitud es " + str(dna_length))
