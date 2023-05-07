2b5c7b9ed4c3fe3a700e5addfb7bac675c542004
anagarme <124300524+anagarme@users.noreply.github.com>
2023-05-07 17:25:01 -0600

    Versión 1.5 del programa secuenciaRNA.py

--- Tareas/Tarea_1/src/secuenciaRNA.py
+++ Tareas/Tarea_1/src/secuenciaRNA.py
@@ -29,24 +29,31 @@ SEE ALSO
 # =                            main
 # ===========================================================================
 
+# Imprimir un mensaje de bienvenida y explicación al usuario
+print("¡Hola! Este programa te permite introducir una secuencia de DNA y este buscará la posición del codón de inicio y el de paro. Asimismo regresará el fragmento de DNA que se transcribirá.")
+
 # Secuencia de DNA en la que se buscará el codón de inicio y de término
-dna = 'AAGGTACGTCGCGCGTTATTAGCCTAAT'
+raw_dna = input("Por favor, introduce tu secuencia de DNA: ")
+dna = raw_dna.upper()
+def secuencia_mRNA(DNA):
+        # Almacenar el codón de inicio
+        codon_inicio = 'TAC'
 
-# Almacenar el codón de inicio
-codon_inicio = 'TAC'
+        # Almacenar el codón de término 
+        codon_paro = 'ATT'
 
-# Almacenar el codón de término 
-codon_paro = 'ATT'
+        # Buscar en la secuencia con el método find los codones de inicio y término respectivamente 
+        inicio = dna.find(codon_inicio)
+        final = dna.find(codon_paro)
 
-# Buscar en la secuencia con el método find los codones de inicio y término respectivamente 
-inicio = dna.find(codon_inicio)
-final = dna.find(codon_paro)
+        # Imprimir las posiciones de los codones de inicio y final
+        print(f'El codón de inicio (TAC) se encuentra en la posición: {inicio}\nEl codón de paro (ATT) está en la posición: {final}')
 
-# Imprimir las posiciones de los codones de inicio y final
-print(f'El codón de inicio (TAC) se encuentra en la posición: {inicio}\n El codón de paro (ATT) está en la posición: {final}')
+        # Extraer la secuencia de DNA que será transcrita 
+        mRNA = dna[inicio:final]
 
-# Extraer la secuencia de DNA que será transcrita 
-mRNA = dna[inicio:final]
+        # Imprimir la secuencia de DNA que será transcrita en RNA
+        print(f'El fragmento de DNA que será trasncrito es: {mRNA}')
 
-# Imprimir la secuencia de DNA que será transcrita en RNA
-print(f'El fragmento de DNA que será trasncrito es: {mRNA}')
+secuencia_mRNA(dna)
+
