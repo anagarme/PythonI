752a5018d0ca9bb889f0b943da72fade7ec65631
anagarme <124300524+anagarme@users.noreply.github.com>
2023-08-09 23:56:12 -0600

    Se agregó el archivo como ejemplo

---
+++ projects/python_class/src/python_class.py
@@ -0,0 +1,26 @@
+'''
+NAME
+	reverse-complement.py
+    
+VERSION
+    2.0
+    
+AUTHOR
+    Ana Marisol, Diego, Camila, Ethan	
+    
+DESCRIPTION
+	Make the reverse complement of DNA sequence
+    
+CATEGORY
+	Genomic Sequence
+    
+USAGE
+
+    % python reverse-complement.py -i 
+    
+    example
+    
+    % python reverse-complement -i
+
+'''
+
